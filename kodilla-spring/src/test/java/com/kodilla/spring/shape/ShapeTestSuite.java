b0db819fb542ede11971879163384ff2f50ac582
michalszmit <michalrd@gmail.com>
2021-12-07 12:58:34 +0100

    Spring Shape

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/shape/ShapeTestSuite.java
@@ -0,0 +1,43 @@
+package com.kodilla.spring.shape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShapeTestSuite {
+
+    @Test
+    void testCircleLoadedIntoContainer() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = context.getBean(Circle.class);
+
+        //When
+        String name = shape.getShapeName();
+
+        //Then
+        assertEquals("This is a circle.", name);
+    }
+
+    @Test
+    void testTriangleLoadedIntoContainer() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = context.getBean(Triangle.class);
+
+        //When
+        String name = shape.getShapeName();
+
+        //Then
+        assertEquals("This is a triangle.", name);
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+}
