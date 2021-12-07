30cf537378fd0b533d11256ca70b8077666f58b9
michalszmit <michalrd@gmail.com>
2021-12-07 15:21:37 +0100

    Modul 14.3, teoria

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -1,30 +1,42 @@
 package com.kodilla.spring.library;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootTest
-class LibraryTestSuite {
-
-    @Autowired
-    private Library library;
-
+public class LibraryTestSuite {
     @Test
     public void testLoadFromDb() {
         //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
         //When
         library.loadFromDb();
         //Then
         //do nothing
     }
-
     @Test
     public void testSaveToDb() {
         //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
         //When
         library.saveToDb();
         //Then
         //do nothing
+    }
+    @Test
+    public void testContext() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        //When & Then
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ===== Beans list ====");
     }
 }
