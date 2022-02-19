efbf60f7e3ba3e61c44c51d1c046778016494090
michalszmit <michalrd@gmail.com>
2022-02-19 18:24:56 +0100

    Modul 24.1 teoria

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
@@ -1,5 +1,9 @@
 package com.kodilla.patterns2.facade;
 
+import com.kodilla.patterns2.facade.api.ItemDto;
+import com.kodilla.patterns2.facade.api.OrderDto;
+import com.kodilla.patterns2.facade.api.OrderFacade;
+import com.kodilla.patterns2.facade.api.OrderProcessingException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +15,8 @@ class ShopServiceTestSuite {
 
     @Autowired
     private ShopService shopService;
+    @Autowired
+    private OrderFacade orderFacade;
 
     @Test
     void testShopServiceSubmitOrder() {
@@ -53,6 +59,20 @@ class ShopServiceTestSuite {
             }
         } else {
             System.out.println("Access denied. User is not authenticated.");
+        }
+    }
+
+    @Test
+    public void testShopFacade() {
+        OrderDto order = new OrderDto();
+        order.addItem(new ItemDto(10L, 2));
+        order.addItem(new ItemDto(216L, 1));
+        order.addItem(new ItemDto(25L, 1));
+        order.addItem(new ItemDto(11L, 3));
+        try {
+            orderFacade.processOrder(order, 1L);
+        } catch (OrderProcessingException e) {
+            // business exception - should be handled in real application
         }
     }
 }
