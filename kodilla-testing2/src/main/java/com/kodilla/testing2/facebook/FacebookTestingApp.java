e16bf00551e9b662d7a5e07cac72996ec59ce661
michalszmit <michalrd@gmail.com>
2022-04-20 16:22:24 +0200

    Zadanie 32.2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -7,15 +7,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestingApp {
-    public static final String XPATH_COOKIE_BTN = "//button[contains(@id, \"u_0_1_4M\")]"; //Zezwól tylko na niezbędne pliki cookie
-    public static final String XPATH_CREATE_BTN = "//a[contains(@id, \"u_0_2_ID\")]"; //Utwórz nowe konto
-    public static final String XPATH_INPUT_NAME = "//div[contains(@id, \"u_g_b_NO\")]"; //Imię
-    public static final String XPATH_INPUT_LASTNAME = "//div[contains(@id, \"u_g_d_+p\")]"; //Nazwisko
+    public static final String XPATH_COOKIE_BTN = "//button[contains(@data-testid, \"cookie-policy-manage-dialog-accept-button\")]"; //Zezwól tylko na niezbędne pliki cookies
+    public static final String XPATH_CREATE_BTN = "//a[contains(@data-testid, \"open-registration-form-button\")]"; //Utwórz nowe konto
+    public static final String XPATH_INPUT_NAME = "//input[contains(@name, \"firstname\")]"; //Imię
+    public static final String XPATH_INPUT_LASTNAME = "//input[contains(@name, \"lastname\")]"; //Nazwisko
     public static final String XPATH_BIRTHDAY_SEL = "//select[contains(@id, \"day\")]"; //Dzień urodzenia
     public static final String XPATH_BIRTH_MONTH_SEL = "//select[contains(@id, \"month\")]"; // Miesiąc urodzenia
     public static final String XPATH_BIRTH_YEAR_SEL = "//select[contains(@id, \"year\")]"; // Rok urodzenia
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.facebook.com/.");
 
@@ -24,6 +24,8 @@ public class FacebookTestingApp {
         WebElement createBtn = driver.findElement(By.xpath(XPATH_CREATE_BTN));
         createBtn.click();
 
+        Thread.sleep(1000);
+
         WebElement inputFirstname = driver.findElement(By.xpath(XPATH_INPUT_NAME));
         inputFirstname.sendKeys("Michal");
         WebElement inputLastname = driver.findElement(By.xpath(XPATH_INPUT_LASTNAME));
@@ -31,10 +33,10 @@ public class FacebookTestingApp {
 
         WebElement daySelect = driver.findElement(By.xpath(XPATH_BIRTHDAY_SEL));
         Select selectBoard1 = new Select(daySelect);
-        selectBoard1.selectByIndex(11);
+        selectBoard1.selectByIndex(7);
         WebElement monthSelect = driver.findElement(By.xpath(XPATH_BIRTH_MONTH_SEL));
         Select selectBoard2 = new Select(monthSelect);
-        selectBoard2.selectByIndex(11);
+        selectBoard2.selectByIndex(7);
         WebElement yearSelect = driver.findElement(By.xpath(XPATH_BIRTH_YEAR_SEL));
         Select selectBoard3 = new Select(yearSelect);
         selectBoard3.selectByIndex(28);
